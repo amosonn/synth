d2c500abc0292953837506417198464c86c92e8c
Amos Onn <amosonn@gmail.com>
2014-06-12 19:07:20 +0300

    using StringIO.

--- pyaudio_sample2.py
+++ pyaudio_sample2.py
@@ -1,12 +1,16 @@
 import pyaudio
 import time
 import sys
+import StringIO
 
 if len(sys.argv) < 3:
     print("Plays a raw file.\n\nUsage: %s filename.wav framerate" % sys.argv[0])
     sys.exit(-1)
 
 f = file(sys.argv[1], 'rb')
+s = f.read()
+f.close()
+sio = StringIO.StringIO(s)
 fr = int(sys.argv[2])
 
 # instantiate PyAudio (1)
@@ -15,7 +19,7 @@ p = pyaudio.PyAudio()
 # define callback (2)
 def callback(in_data, frame_count, time_info, status):
     print(in_data, frame_count, time_info, status) 
-    data = f.read(2*frame_count)
+    data = sio.read(2*frame_count)
     return (data, pyaudio.paContinue)
 
 # open stream using callback (3)
@@ -35,7 +39,6 @@ while stream.is_active():
 # stop stream (6)
 stream.stop_stream()
 stream.close()
-f.close()
 
 # close PyAudio (7)
 p.terminate()
