bf4a3acf76c4ca451628abc943e5be423193b9bc
Amos Onn <amosonn@gmail.com>
2014-06-28 22:32:54 +0300

    Added test requiring user input.

---
+++ test_sine.py
@@ -0,0 +1,11 @@
+from out.player import Player
+from iters.sine_iter import sine_iter
+from iters.num_to_str_iter import num_to_str_iter
+from streams.str_iter_stream import StrIterStream
+
+def test_sine():
+    p = Player()
+    si = sine_iter(2,440)
+    sis = StrIterStream(num_to_str_iter(si))
+    p.play(sis)
+    assert raw_input("Did you hear a sound? [N/y]") in "yY"
