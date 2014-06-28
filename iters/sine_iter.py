cf0511df1b8d6736e6a8eb9e81afedbaf3ebec36
Amos Onn <amosonn@gmail.com>
2014-06-28 23:26:25 +0300

    Fixed kwargs in lambda of length.

--- iters/sine_iter.py
+++ iters/sine_iter.py
@@ -5,7 +5,7 @@ import math
 
 from synth.decorators import length
 
-@length(lambda length,f,a=0,samprate=44100: length*samprate)
+@length(lambda length,freq,amp=1,samprate=44100: length*samprate)
 def sine_iter(length,freq,amp=1,samprate=44100):
     """
     A string iterator that returns a 2-bytes representation of a sine wave,
@@ -15,4 +15,4 @@ def sine_iter(length,freq,amp=1,samprate=44100):
     k1 = float(freq)*2*math.pi/samprate
     k2 = 0x7fff*amp
     for i in xrange(int(length*samprate)):
-        yield (int(math.sin(i*k1)*k2 + 0.5),)
+        yield [int(math.sin(i*k1)*k2 + 0.5)],1
