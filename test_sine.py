3b37f433f0c4673178cc8e0cebf8ee152250b7be
Amos Onn <amosonn@gmail.com>
2014-06-28 23:25:06 +0300

    Updated test.
    No raw_input, and sine not at full power.

--- test_sine.py
+++ test_sine.py
@@ -1,3 +1,5 @@
+import time
+
 from synth.out.player import Player
 from synth.iters.sine_iter import sine_iter
 from synth.iters.num_to_str_iter import num_to_str_iter
@@ -5,7 +7,9 @@ from synth.streams.str_iter_stream import StrIterStream
 
 def test_sine():
     p = Player()
-    si = sine_iter(2,440)
+    si = sine_iter(2,440,amp=0.3)
     sis = StrIterStream(num_to_str_iter(si))
     p.play(sis)
-    assert raw_input("Did you hear a sound? [N/y]") in "yY"
+    time.sleep(2)
+
+    #assert raw_input("Did you hear a sound? [N/y]") in "yY"
