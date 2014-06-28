bdcf7f44f74fba8c81b6d3f26d06780d4b342a36
Amos Onn <amosonn@gmail.com>
2014-06-28 22:17:27 +0300

    Separated num_to_str from sine.
    Added decorator wrapper for num_to_str.

--- iters/num_to_str_iter.py
+++ iters/num_to_str_iter.py
@@ -1,9 +1,21 @@
 """
 num_to_str_iter
 """
-def num_to_str_iter(i):
+class num_to_str_iter(object):
     """
     Takes a 16bit unsigned number iterator, and yields for each value,
     a tuple of 2bytes representation of the number (shifted by 0x8000 up),
     and the length.
     """
+    def __init__(self,iter):
+        self._iter = iter
+
+    def __len__(self):
+        return 2 * len(self._iter)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        x = self._iter.next()
+        return (chr(x&0xff) + chr(((x>>8)&0xff)^0x80),2)
