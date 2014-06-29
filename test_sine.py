b149ad564a73f9de895de7dc783522bda30960cc
Amos Onn <amosonn@gmail.com>
2014-06-29 17:08:56 +0300

    Added test for two sines.

--- test_sine.py
+++ test_sine.py
@@ -3,6 +3,7 @@ import time
 from synth.out.player import Player
 from synth.iters.sine_iter import sine_iter
 from synth.iters.num_to_str_iter import num_to_str_iter
+from synth.iters.coeff_sum_iter import coeff_sum_iter
 from synth.streams.str_iter_stream import StrIterStream
 
 def test_sine():
@@ -13,3 +14,12 @@ def test_sine():
     time.sleep(2)
 
     #assert raw_input("Did you hear a sound? [N/y]") in "yY"
+
+def test_two_sines():
+    p = Player()
+    si440 = sine_iter(2,440)
+    si660 = sine_iter(2,660)
+    sis = StrIterStream(num_to_str_iter(coeff_sum_iter( \
+        (0.5,si440),(0.5,si660))))
+    p.play(sis)
+    time.sleep(2)
