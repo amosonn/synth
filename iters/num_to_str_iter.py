b77a9d5d00a319c2533d460b9023a86f09bb46e5
Amos Onn <amosonn@gmail.com>
2014-06-29 16:24:46 +0300

    Cleaned num_to_str_iter to use @length.

--- iters/num_to_str_iter.py
+++ iters/num_to_str_iter.py
@@ -1,20 +1,13 @@
 """
 num_to_str_iter
 """
-class num_to_str_iter(object):
+from synth.decorators import length
+
+@length(lambda iter: 2 * len(iter))
+def num_to_str_iter(iter):
     """
     Takes a 16bit unsigned number iterator, and yields for each value,
     a tuple of 2bytes representation of the number (shifted by 0x8000 up).
     """
-    def __init__(self,iter):
-        self._iter = iter
-
-    def __len__(self):
-        return 2 * len(self._iter)
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        vals = self._iter.next()
-        return "".join([chr(x&0xff) + chr(((x>>8)&0xff)^0x80) for x in vals])
+    for vals in iter:
+        yield "".join([chr(x&0xff) + chr(((x>>8)&0xff)^0x80) for x in vals])
