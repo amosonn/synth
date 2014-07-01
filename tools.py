a365e3f5e1ba5ad4f38fe4eb9d36df895c99936f
Amos Onn <amosonn@gmail.com>
2014-07-01 21:59:26 +0300

    Got buffer_stream off floating-point fracs.

--- tools.py
+++ tools.py
@@ -20,17 +20,19 @@ def buffer_stream(stream,length,frac):
     """
     Assuming only a given length of a given stream is wanted, buffers
     a fraction of it, and keeps reading at a lower pace from the original
-    stream.
+    stream. Rounds frac to the nearst 1/1000, to escape floating-point
+    errors.
     """
+    n = int(frac*1000)
     stream.limit(length)
-    to_read = length*frac
-    int_to_read = int(to_read)
-    rem = to_read - int_to_read
+    to_read_1000 = length*n
+    int_to_read = to_read_1000 / 1000
+    rem = to_read_1000 % 1000
     buf = stream.take(int_to_read)
-    op_frac = 1 - frac
+    op_n = 1000 - n
     while True:
-        rem += op_frac
-        if rem >= 1:
+        rem += op_n
+        if rem >= 1000:
             try:
                 val = stream.take()
             except StopIteration:
@@ -38,7 +40,7 @@ def buffer_stream(stream,length,frac):
                     yield x
                 break
             buf.append(val)
-            rem -= 1
+            rem -= 1000
         yield buf[0]
         del buf[0]
 
